b77c50698940b1bc5f931e3ff3f38c116e2450e9
Biuni <info.gianlucabonifazi@gmail.com>
2015-04-01 12:22:46 +0200

    Create cssdebugger.js

---
+++ cssdebugger.js
@@ -0,0 +1,19 @@
+/*
+* Create by Gianluca Bonifazi
+* www.biuni.it
+* Copyright - MIT LICENSE
+*/
+
+function getRandomColor() {
+    var letters = '0123456789ABCDEF'.split('');
+    var color = "#";
+    for (var i = 0; i < 6; i++ ) {
+        color += letters[Math.floor(Math.random() * 16)];
+    }
+    return color;
+} //Randomize Color Of Outline
+ 
+var myList = document.getElementsByTagName("*");
+for (var i = 0; i < myList.length; i++) {
+        myList[i].style.outline = '2px solid '+getRandomColor();
+} // Outline All Element
